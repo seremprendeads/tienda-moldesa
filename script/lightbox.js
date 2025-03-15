095fed6435f8f42a7d39cdf60c178de3f316798d
seremprendeads <seremprendeads@gmail.com>
2025-03-14 21:40:05 -0300

    Update lightbox.js

--- script/lightbox.js
+++ script/lightbox.js
@@ -1,1 +1,18 @@
-closeLightbox.addEventListener("click",()=>{lightboxModal.style.display="none"}),lightboxModal.addEventListener("click",l=>{l.target===lightboxModal&&(lightboxModal.style.display="none")}),document.addEventListener("keydown",l=>{"Escape"===l.key&&"flex"===lightboxModal.style.display&&(lightboxModal.style.display="none")});
+// Close Lightbox when clicking on X
+closeLightbox.addEventListener('click', () => {
+    lightboxModal.style.display = 'none';
+});
+
+// Close Lightbox when clicking outside the image
+lightboxModal.addEventListener('click', (e) => {
+    if (e.target === lightboxModal) {
+        lightboxModal.style.display = 'none';
+    }
+});
+
+// Close Lightbox with Escape key
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && lightboxModal.style.display === 'flex') {
+        lightboxModal.style.display = 'none';
+    }
+});
